48bb01680b4134388551b07a17cdcb8f947de03d
lin.hanchao <lin.hanchao@byd.com>
2023-02-06 18:06:00 +0800

    🎉 init: 初始化扩展

---
+++ src/devtools/devtools.ts
@@ -0,0 +1,25 @@
+
+
+chrome.devtools.panels.create("StorageEditor", "icon.png", "panel.html", panel => {
+    // code invoked on panel creation
+    panel.onShown.addListener( (extPanelWindow) => {
+        let sayHello = extPanelWindow.document.querySelector('#sayHello');
+        sayHello?.addEventListener("click", () => {
+            // show a greeting alert in the inspected page
+            chrome.devtools.inspectedWindow.eval('alert("Hello from the DevTools Extension");');
+        });             
+    });
+});
+
+// Create a connection to the background service worker
+const backgroundPageConnection = chrome.runtime.connect({
+    name: "devtools-page"
+});
+
+// Relay the tab ID to the background service worker
+backgroundPageConnection.postMessage({
+    name: 'init',
+    tabId: chrome.devtools.inspectedWindow.tabId
+});
+
+export {};
---
+++ src/options/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <p>Options2</p>
+</template>
+
+<style>
+</style>
