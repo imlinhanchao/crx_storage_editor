40c5f9e92b338c7f5310be90b896bf98ac8c6fd0
Hancel.Lin <imlinhanchao@gmail.com>
2023-02-08 22:23:16 +0800

    🐞 fix: 解决不同Tab数据显示相互干扰问题

--- src/devtools/main.ts
+++ src/devtools/main.ts
@@ -1,45 +1,39 @@
 
-let panelWindow: Window | null = null;
+let panelWindow: { [key:number]: Window} = {};
 chrome.devtools.panels.create("StorageEditor", "icon.png", "src/panel/index.html", panel => {
   // code invoked on panel creation
   panel.onShown.addListener( (extPanelWindow) => {
-    panelWindow = extPanelWindow;
+    panelWindow[chrome.devtools.inspectedWindow.tabId] = extPanelWindow;
     chrome.devtools.inspectedWindow.eval(`
-      let _setItem = Storage.prototype.setItem;
-      Storage.prototype.setItem = function(keyName, keyValue) {
-        postStorage(this)
-        return _setItem.call(this, keyName, keyValue)
-      }
-      function postStorage(storage) {
-        window.postMessage({
-          from: 'tab_storage_editor',
-          message: 'storage',
-          data: storage == localStorage ? 'local' : 'session'
-        }, '*')
-      }
-      window.addEventListener('message', function({ data }) {
-        if (data && data.from === '__content_script_storage_editor') {
-          switch(data.message) {
-            case 'listen':
-              window.addEventListener('storage', (e) => {
-                postStorage(e.storageArea)
-              }
-            )
-          }
+      if (!window.postStorage) {
+        let _setItem = Storage.prototype.setItem;
+        Storage.prototype.setItem = function(keyName, keyValue) {
+          postStorage(this)
+          return _setItem.call(this, keyName, keyValue)
         }
-      });
+        window.postStorage = (storage) => {
+          window.postMessage({
+            from: 'tab_storage_editor',
+            message: 'storage',
+            data: storage == localStorage ? 'local' : 'session'
+          }, '*')
+        }
+        window.addEventListener('message', function({ data }) {
+          if (data && data.from === '__content_script_storage_editor') {
+            switch(data.message) {
+              case 'listen':
+                window.addEventListener('storage', (e) => {
+                  postStorage(e.storageArea)
+                }
+              )
+            }
+          }
+        });
+      }
+      postStorage(localStorage)
+      postStorage(sessionStorage)
     `)
 
-    chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, 
-      { from: '__devtools_storage_editor', message: 'send', data: 'local' }, 
-      function(response) {
-        console.log('send response', response);
-    });
-    chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, 
-      { from: '__devtools_storage_editor', message: 'send', data: 'session' }, 
-      function(response) {
-        console.log('send response', response);
-    });
     chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, 
       { from: '__devtools_storage_editor', message: 'listen' }, 
       function(response) {
@@ -50,14 +44,19 @@ chrome.devtools.panels.create("StorageEditor", "icon.png", "src/panel/index.html
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // Messages from content scripts should have sender.tab set
-  if (sender.tab && panelWindow) {
-    panelWindow.postMessage({ ...request, from: '__devtools_storage_editor'});
+  if (sender.tab && sender.tab.active 
+    && chrome.devtools.inspectedWindow.tabId == sender.tab.id 
+    && panelWindow) {
+    console.log('sender', sender.tab);
+    console.log('id', chrome.devtools.inspectedWindow.tabId)
+    panelWindow[chrome.devtools.inspectedWindow.tabId].postMessage({ ...request, from: '__devtools_storage_editor'});
     sendResponse({
       result: 'success'
     })
   }
 });
 
+chrome.tabs.onActivated
 
 // Create a connection to the background service worker
 const backgroundPageConnection = chrome.runtime.connect({
