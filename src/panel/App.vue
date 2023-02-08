66cc54fa686d70563f0fab3192fda1d7d4b1e731
Hancel.Lin <imlinhanchao@gmail.com>
2023-02-08 23:37:13 +0800

    ✨ feat: 添加编辑功能和样式

--- src/panel/App.vue
+++ src/panel/App.vue
@@ -2,6 +2,7 @@
 import { computed, ref } from 'vue';
 import { useDark, useToggle } from '@vueuse/core'
 import { isArray, isBoolean, isNull, isNumber, isObject, isString, isUnDef } from './utils/is';
+import { EditPen } from '@element-plus/icons-vue';
 
 const isDark = useDark()
 useToggle(isDark)
@@ -11,24 +12,44 @@ const storage = ref<any>({
   local: {} as any,
   session: {} as any,
 });
+const historyStorage = ref<any>({
+  local: {} as any,
+  session: {} as any,
+});
 
 window.addEventListener('message', (ev) => {
   let request = ev.data;
   if (request.from != '__devtools_storage_editor') return;
   switch(request.message) {
     case 'update':
-      storage.value[request.type] = parseStorage(request.data);
+      updateStorage(request);
       break;
   }
 })
 
-function parseStorage(s: any) {
-  Object.keys(s).forEach(k => {
-    try {
-      let data = JSON.parse(s[k]);
-      if (!isString(data)) s[k] = data;
-    } catch (error) {}
+function updateStorage({type, data}: { type: string, data: any }) {
+  let oldStorageValue = historyStorage.value[type];
+  let oldStorage = storage.value[type];
+
+  let newKeys = Object.keys(data);
+  let oldKeys = Object.keys(oldStorage);
+  oldKeys.forEach(k => {
+    if (!newKeys.includes(k)) delete oldStorage[k];
+    else if (oldStorageValue[k] != data[k]) oldStorage[k] = parseStorage(data[k]);
   })
+
+  newKeys.forEach(k => {
+    if (!oldKeys.includes(k)) oldStorage[k] = parseStorage(data[k]);
+  })
+
+  oldStorageValue = data;
+}
+
+function parseStorage(s: string) {
+  try {
+    let data = JSON.parse(s);
+    if (!isString(data)) s = data;
+  } catch (error) {}
   return s;
 }
 
@@ -40,12 +61,16 @@ const sessionTree = computed(() =>
 )
 function dataToTree(data: any, key: string, path: string): any {
   if (isObject(data)) return { 
-    key, type: 'Object', path, children: Object.keys(data).map(k => dataToTree(data[k], k, path + '.' + k)) 
+    key, type: 'Object', path, children: Object.keys(data)
+      .map(k => Object.assign({ parent: data }, dataToTree(data[k], k, path + '.' + k))) 
   };
   else if (isArray(data)) return {
     key, type: 'Array', path, children: data.map((d, i) => dataToTree(d, i + '', path + '.' + i)) 
   }
-  else return { key, path, data, children: [] } 
+  else return { key, __key: key, path, data, view: toView(data), children: [] } 
+}
+function toView(data: any) {
+  return JSON.stringify(data);
 }
 </script>
 
@@ -57,20 +82,27 @@ function dataToTree(data: any, key: string, path: string): any {
     <el-tab-pane label="Local Storage" name="local">
       <el-tree
         :data="localTree"
-        node-key="id"
-        default-expand-all
+        node-key="path"
         :expand-on-click-node="false"
       >
         <template #default="{ node, data }">
           <span class="data-tree-node">
-            <b>{{ data.key }}</b> : 
+            <b class="key" v-if="!data.edit">{{ data.key }}</b>
+            <el-input class="key" type="text" v-model="data.key" v-if="data.edit" size="small"/>
+            <span class="sp">: </span>
             <span v-if="data.type">{{ data.type }}</span> 
-            <span v-else-if="isString(data.data)" class="data-string">"{{ data.data }}"</span>
-            <span v-else-if="isNumber(data.data)" class="data-number">{{ data.data }}</span>
-            <span v-else-if="isBoolean(data.data)" class="data-boolean">{{ data.data }}</span>
-            <span v-else-if="isNull(data.data)" class="data-null">null</span>
-            <span v-else-if="isUnDef(data.data)" class="data-undefined">undefined</span>
-            <span v-else class="data-empty">{{ data.data }}</span>
+            <span v-else class="value" v-if="!data.edit">
+              <span v-if="isString(data.data)" class="data-string">"{{ data.data }}"</span>
+              <span v-else-if="isNumber(data.data)" class="data-number">{{ data.data }}</span>
+              <span v-else-if="isBoolean(data.data)" class="data-boolean">{{ data.data }}</span>
+              <span v-else-if="isNull(data.data)" class="data-null">null</span>
+              <span v-else-if="isUnDef(data.data)" class="data-undefined">undefined</span>
+              <span v-else class="data-empty">{{ data.data }}</span>
+            </span>
+            <el-input type="text" v-model="data.view" v-if="data.edit" size="small"/>
+            <span class="action" v-if="!data.type">
+              <el-button type="primary" :icon="EditPen" size="small" link @click="data.edit = true" />
+            </span>
           </span>
         </template>
       </el-tree>
@@ -84,7 +116,7 @@ function dataToTree(data: any, key: string, path: string): any {
       >
         <template #default="{ node, data }">
           <span class="data-tree-node">
-            <b>{{ data.key }}</b> : 
+            <b class="key">{{ data.key }}</b> : 
             <span v-if="data.type">{{ data.type }}</span> 
             <span v-else-if="isString(data.data)" class="data-string">"{{ data.data }}"</span>
             <span v-else-if="isNumber(data.data)" class="data-number">{{ data.data }}</span>
@@ -102,6 +134,13 @@ function dataToTree(data: any, key: string, path: string): any {
 <style>
 body {
   background-color: var(--el-bg-color);
+  font-family: 'Roboto Mono', Menlo, Consolas, monospace;
+}
+* {
+  font-family: inherit;
+}
+.el-input__inner {
+  color: inherit;
 }
 .el-tabs__content {
   overflow: auto;
@@ -127,5 +166,19 @@ body {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+  display: flex;
+  align-items: center;
+}
+.data-tree-node .key {
+  color: #cea3f9;
+}
+.data-tree-node .sp {
+  margin-right: .5em;
+}
+.action {
+  display: none;
+}
+.data-tree-node:hover .action {
+  display: inline-block;
 }
 </style>
--- src/popup/main.ts
+++ src/popup/main.ts
@@ -3,4 +3,10 @@ import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).use(ElementPlus).mount('#app')
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(ElementPlus).mount('#app')
