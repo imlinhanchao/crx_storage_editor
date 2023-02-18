1107b8dfbf24d281792a2987b6ee0c7e00513400
Hancel.Lin <imlinhanchao@gmail.com>
2023-02-18 13:46:39 +0800

    🐞 fix: 修正预载样式

--- src/panel/views/Home.vue
+++ src/panel/views/Home.vue
@@ -1,16 +1,6 @@
 <script lang="ts" setup></script>
 <template>
-  <img id="logo" src="/logo.png" />
+  <img id="preload" src="/logo.png" />
 </template>
 <style lang="scss" scoped>
-img#logo {
-  width: 128px;
-  height: 128px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  margin: auto;
-}
 </style>
