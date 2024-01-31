4a3363dc9de136e5d6ce22d390104b0934784051
Hancel.Lin <imlinhanchao@gmail.com>
2024-01-31 16:52:58 +0800

    🐞 fix: 修正typescript检查错误

--- src/panel/main.ts
+++ src/panel/main.ts
@@ -2,6 +2,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import { IconPack } from "@fortawesome/fontawesome-svg-core";
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
@@ -15,8 +16,8 @@ import { useDark, useToggle } from '@vueuse/core'
 const isDark = useDark()
 useToggle(isDark);
 
-library.add(fas);
-library.add(far);
+library.add(fas as IconPack);
+library.add(far as IconPack);
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
